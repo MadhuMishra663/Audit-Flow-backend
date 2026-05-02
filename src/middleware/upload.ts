218ac00ce7962dcd9a6a25666e6ac10b627e8d02
MadhuMishra663 <madhu@byldd.com>
2026-05-02 22:07:53 +0530

    wip

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,20 +1,32 @@
-import multer from "multer";
+import multer, { StorageEngine } from "multer";
+import { Request } from "express";
 import fs from "fs";
+import path from "path";
 
 const uploadDir = "uploads";
 
 // ensure folder exists
 if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
+  fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+const storage: StorageEngine = multer.diskStorage({
+  destination: (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, destination: string) => void,
+  ) => {
     cb(null, uploadDir);
   },
-  filename: (req, file, cb) => {
+
+  filename: (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, filename: string) => void,
+  ) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
+
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
   },
 });
 
